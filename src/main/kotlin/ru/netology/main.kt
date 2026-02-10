5b279b8c11e807428b3d761231e0074b752dd14f
Dm Kv <demon1st@yandex.ru>
2026-02-11 01:43:59 +0300

    task 1

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -20,19 +20,21 @@ class WallService {
     private var posts = emptyArray<Post>()
     private var comments = emptyArray<Comment>()
 
-    fun findById(postId: Int) : Post? {
+    fun findById(postId: Int) : Boolean {
         for ((index, post) in posts.withIndex()) {
             if (postId == post.id) {
-                return posts[postId]
+                return true
             }
         }
-        return null
+        return false
     }
 
     fun createComment(postId: Int, comment: Comment): Comment {
         val service = WallService()
-        comments[postId] = service.findById(postId)? : throw PostNotFoundException ("No posts with this id $postId")
-        return comment
+        if (service.findById(postId)) {
+            comments[postId] = comment.copy()
+            return comment
+        } else throw PostNotFoundException ("No posts with this id $postId")
     }
 }
 
